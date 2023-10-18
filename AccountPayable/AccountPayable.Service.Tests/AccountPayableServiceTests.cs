c4e671ea7e5f6746d06485c53b6ca71b193d42f4
Andrey Tsygankov <tsygankov@gmail.com>
2023-10-18 11:43:03 -0400

    mock data for testing

--- AccountPayable/AccountPayable.Service.Tests/AccountPayableServiceTests.cs
+++ AccountPayable/AccountPayable.Service.Tests/AccountPayableServiceTests.cs
@@ -7,11 +7,19 @@ namespace AccountPayable.Service.Tests;
 public class AccountPayableServiceTests
 {
     IVendorRepository vendors;
+    IPaymentMethodRepository paymentMethods;
+    IBillRepository bills;
+    IPaymentRepository payments;
+
     public AccountPayableServiceTests()
     {
         // run fixture
 
         vendors = Fixture.createVendorRepo();
+        paymentMethods = Fixture.createPaymentMethodRepo();
+        bills = Fixture.createBillRepo();
+        payments = Fixture.createPaymentRepo();
+
     }
 
     [Fact]

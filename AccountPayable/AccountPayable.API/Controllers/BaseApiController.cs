00274228890515d87d5d1deb39b297b2a83aa083
Andrey Tsygankov <tsygankov@gmail.com>
2023-10-18 09:01:50 -0400

    rearrange projects in solution

---
+++ AccountPayable/AccountPayable.API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountPayable.API.Controllers
+{
+    [Route("api/[controller]")]
+    //[TypeFilter(typeof(AuthorizationFilterAttribute))]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
+

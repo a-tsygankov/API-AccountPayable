9bc6c648bacd728c8ca72fcacb34c3eb9a17abe5
Andrey Tsygankov <tsygankov@gmail.com>
2023-10-18 12:14:18 -0400

    Read model to see the bill with all details in text form

---
+++ AccountPayable/AccountPayable.Service/ReadModels/BillModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace AccountPayable.Service.ReadModels
+{
+	public class BillModel
+	{
+        public long Id { get; set; }
+
+        public required string OrderOf { get; set; }
+
+        public long AccountId { get; set; }
+
+        public string VendorName { get; set; }
+
+        public string MaymentMethodName { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool Paid { get; set; }
+    }
+}
+

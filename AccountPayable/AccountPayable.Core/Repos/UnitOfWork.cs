1778f1819724db0bbab0db568799df3ece052d42
Andrey Tsygankov <tsygankov@gmail.com>
2023-10-18 02:31:21 -0400

    db scripts

---
+++ AccountPayable/AccountPayable.Core/Repos/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AccountPayable.Core.Interfaces;
+
+namespace AccountPayable.Core.Repos
+{
+	public class UnitOfWork : IUnitOfWork
+
+	{
+		public UnitOfWork()
+		{
+		}
+
+        public IVendorRepository Vendors => throw new NotImplementedException();
+
+        public IPaymentMethodRepository PaymentMethods => throw new NotImplementedException();
+
+        public IBillRepository Bills => throw new NotImplementedException();
+
+        public IPaymentRepository Payments => throw new NotImplementedException();
+    }
+}
+
--- AccountPayable/AccountPayable.Service.Sql/Scripts/Payment.sql
+++ AccountPayable/AccountPayable.Service.Sql/Scripts/Payment.sql
@@ -8,17 +8,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Bill](
+CREATE TABLE [dbo].[Payment](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[OrderOf] [nvarchar](200) NOT NULL,
 	[AccountId] [int] NOT NULL, # account numbers are not in this system
-	[VendorId] [int] NOT NULL, # technically, it should be a FK to VENDOR table
+	[BillId] [int] NOT NULL, # technically, it should be a FK to VENDOR table
 	[Amount] [decimal(10, 2)] NOT NULL,
-	[DueDate] [date] NULL,
-	[Paid] [int] DEFAULT 0
+	[PaymentDate] [date] NOT NULL,
+	[PaymentMethodId] [int] NOT NULL
 
 
- CONSTRAINT [PK_Bill] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Payment] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]

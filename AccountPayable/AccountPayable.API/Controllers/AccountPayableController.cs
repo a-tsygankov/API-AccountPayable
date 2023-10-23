757b7ac7561a46465a16fd5474c9150e1d83bf20
Andrey Tsygankov <tsygankov@gmail.com>
2023-10-22 23:37:19 -0400

    fixed

--- AccountPayable/AccountPayable.API/Controllers/AccountPayableController.cs
+++ AccountPayable/AccountPayable.API/Controllers/AccountPayableController.cs
@@ -9,21 +9,23 @@ namespace AccountPayable.API.Controllers;
 public class AccountPayableController : ControllerBase
 {
     private readonly IAccountPayableService _service;
+    private readonly IReadModelService _readModelService;
     private readonly ILogger<AccountPayableController> _logger;
 
-    public AccountPayableController(IAccountPayableService service, ILogger<AccountPayableController> logger)
+    public AccountPayableController(IAccountPayableService service, IReadModelService readModelService, ILogger<AccountPayableController> logger)
     {
         _service = service;
+        _readModelService = readModelService;
         _logger = logger;
     }
 
     [HttpGet(Name = "GetBills")]
-    public async Task<IEnumerable<BillRM>> Get(long? accountId, long? vendorId, bool isPaid = false)
+    public async Task<IEnumerable<BillRM>> QueryBills(long? accountId, long? vendorId, bool isPaid = false)
     {
         var bills = await _service.QueryBillsAsync(accountId, vendorId, isPaid);
-        var billRMs = 
+        var billRMs = await _readModelService.GetBillReadModelAsync((IList<Core.Entities.Bill>)bills);
 
-        return Array.Empty<BillRM>();
+        return billRMs;
     }
 }
 
